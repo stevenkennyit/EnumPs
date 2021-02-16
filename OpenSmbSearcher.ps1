9b56a149f5113340006a64f17d8786684af6e5db
Steven <stevenkenny.it@gmail.com>
2021-02-16 06:43:57 -0500

    Add files via upload

---
+++ OpenSmbSearcher.ps1
@@ -0,0 +1,24 @@
+﻿#Script used to identify SMB shares by querying Active Directory then checking each computer for Shared Folders using "net view"
+
+#Enter target domain here
+$domain = "FakeAdDomain.com"
+
+$ErrorActionPreference = "SilentlyContinue"
+$computers = Get-ADComputer -filter * -Server $domain 
+
+foreach($c in $computers){
+    Clear-Variable check -ErrorAction SilentlyContinue
+    $target = $c.DNSHostName
+    if($c.dnshostname){
+        $shared = (net view \\$target) | % { if($_.IndexOf(' Disk ') -gt 0){ $_.Split('  ')[0] } }
+        ""
+        $target
+        foreach($s in $shared){
+            $filepath = "\\"+$target+"\"+$s
+            $check = gci $filepath
+            $c | select @{Name="Server";Expression={$target}}, @{Name="Path";Expression={$filepath}}, @{Name="Files";Expression={$check.count}}
+            #$c | select @{Name="Server";Expression={$target}}, @{Name="Path";Expression={$filepath}}, @{Name="Files";Expression={$check.count}} | Export-Csv c:\temp\OpenFileShares.csv -NoTypeInformation -Append
+        }
+    }
+}
+
